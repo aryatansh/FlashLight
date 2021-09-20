f0c7c31496dbff2351bc0bd261eeffaf1415e961
Aryat Anoop Srivastava <aryatsrivastava06@gmail.com>
2021-09-20 22:57:22 +0530

    Added React Native Package and torch functionality

--- App.js
+++ App.js
@@ -1,111 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import Torch from 'react-native-torch';
+import React, {useState, useEffect} from 'react';
+import {Button, View, StyleSheet, TouchableOpacity, Text} from 'react-native';
+const App: () => Node = () => {
+  const [isOn, setIsOn] = useState(false);
+  const [torchButtonText, setTorchButtonText] = useState('ON');
+  useEffect(() => {
+    Torch.switchState(isOn);
+    if (isOn) {
+      setTorchButtonText('OFF');
+    } else {
+      setTorchButtonText('ON');
+    }
+  });
+  const switchState = () => {
+    setIsOn(!isOn);
+  };
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={switchState} style={styles.button}>
+        <Text style={isOn ? styles.headerTextTrue : styles.headerTextFalse}>
+          {torchButtonText}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  headerTextFalse: {
+    fontSize: 100,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'green',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  headerTextTrue: {
+    fontSize: 100,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'red',
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    width: 200,
+    height: 200,
+    justifyContent: 'center',
+    borderRadius: 100,
+    alignItems: 'center',
+    backgroundColor: '#000000',
+    padding: 10,
   },
 });
 
